373ace7ed564a189f6010423b99b826e75892da3
Thang Nguyen <nguyenhongthang1998@gmail.com>
2021-03-13 19:13:15 -0500

    Fix app not running on real device

--- app/src/main/java/com/neurondigital/selfcare/MainActivity.java
+++ app/src/main/java/com/neurondigital/selfcare/MainActivity.java
@@ -1,9 +1,12 @@
 package com.neurondigital.selfcare;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.icu.util.ULocale;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -15,6 +18,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -50,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         activity = this;
         context = this;
-
+        verifyStoragePermissions(this);
         //enable/disable Firebase topic subscription
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         if (sharedPref.getBoolean("pref_enable_push_notifications", true))
@@ -202,6 +206,26 @@ public class MainActivity extends AppCompatActivity {
                     fragment.onRequestPermissionsResult(requestCode, permissions, grants);
                 }
             }
+        }
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
         }
     }
 }
