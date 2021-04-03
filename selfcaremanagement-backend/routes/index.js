34172609a010fc615ea211931330783fdaf615fd
Thang Nguyen <nguyenhongthang1998@gmail.com>
2021-04-03 15:35:03 -0400

    Remove comments

--- selfcaremanagement-backend/routes/index.js
+++ selfcaremanagement-backend/routes/index.js
@@ -78,11 +78,10 @@ router.post('/sync', authMiddleware, async (req, res, next) => {
 })
 
 router.get('/:username', async (req, res, next) => {
-  var username = req.params.username; // admin
+  var username = req.params.username;
   if(!username) return res.status(400);
 
   var user = await User.findOne({ username: username });
-  console.log(user);
   if(!user) return res.status(400);
   var mldRecords = await ManualLymphDrainageMassage.find({ user: user._id });
   var scRecords = await Skincare.find({ user: user._id });
